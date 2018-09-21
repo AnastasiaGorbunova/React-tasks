050f634ffcd95dcd538f19f7c4d147da71c78fbb
Anastasia Gorbunova <anastasiaharbunova@gmail.com>
2018-09-21 14:01:49 +0300

    react methods

--- component-task/src/component.js
+++ component-task/src/component.js
@@ -3,21 +3,52 @@ const e = React.createElement;
 class myComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            steps: ['constructor',
+                'getDerivedStateFromProps',
+                'render',
+                'componentDidMount']
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    static getDerivedStateFromProps(prop, state) {
+        return null;
     }
 
     render() {
-        const steps = ['constructor', 'getDerivedStateFromProps', 'render', 'componentDidMount'];
-        const listSteps = steps.map((step) =>
+        const listSteps = this.state.steps.map((step) =>
             <li key={step.toString()}>
                 {step}
             </li>
         );
 
-
         return (
-            <ul>{listSteps}</ul>
+            <ul className='list'>{listSteps}</ul>
         );
     }
+
+    handleClick() {
+        const steps = this.state.steps;
+        const item = 'componentDidUpdate';
+        this.setState({steps: steps.concat(item)});
+        console.log('kek');
+    }
+
+    componentDidMount() {
+        const list = document.querySelector('.list');
+        list.addEventListener('click', this.handleClick);
+        console.log(this.state);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // list.removeEventListener('click', this.g);
+        console.log(this.state);
+    }
+
+    // componentDidUnmount(){
+    //
+    // }
 }
 
 const domContainer = document.querySelector('.app-container');
