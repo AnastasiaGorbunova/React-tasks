33be99f3652d141dc536a1e348360111114b999f
Anastasia Gorbunova <anastasiaharbunova@gmail.com>
2018-10-03 14:50:42 +0300

    added book list

--- book-library/src/app.js
+++ book-library/src/app.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../assets/main.scss';
+import {Provider} from 'react-redux';
+import {connect} from 'react-redux';
+import {createStore} from 'redux';
 
+const initialState = [
+    {
+        author: 'Jack London',
+        year: 1990,
+        str: 234
+    },
+    {
+        author: 'Lilo Stich',
+        year: 1978,
+        str: 99
+    }
+];
+
+function AuthorsReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD_TRACK':
+            return state.concat([action.payload]);
+        default:
+            return state
+    }
+}
+
+const store = createStore(AuthorsReducer);
 class App extends React.Component {
+
     render() {
+        const d = store.getState();
         return (
-          <div>Lalka</div>
+            <Provider store={store}>
+                <ul className='list'> {
+                    d.map((step, index) =>
+                        <li key={index}>
+                            {step.author}
+                        </li>
+                    )
+                } </ul>
+            </Provider>
         );
     }
 }
+
+
+
+
 
 const domContainer = document.querySelector('.app-container');
 ReactDOM.render(<App/>, domContainer);
---
+++ book-library/src/containers/bookList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class BookList extends React.Component {
+    render() {
+        return (
+
+        );
+    }
+}
+
+const domContainer = document.querySelector('.app-container');
+ReactDOM.render(<BookList/>, domContainer);
+
+export default BookList;
